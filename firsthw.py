6f15d8120fcca8616aa0a4da3d604086c50c03d3
enidzheng <52223959+enidzheng@users.noreply.github.com>
2019-07-02 15:57:18 +1000

    Add files via upload

--- firsthw.py
+++ firsthw.py
@@ -3,22 +3,24 @@ import platform
 print("please input a number")
 try:
     n = int(input())
-    print (f"your input is int {n}")
+    #print (f"your input is int {n}")
     if (n>0): 
     #print("Your input is number")
-     L=n
-     L = int(L)
-     s=L-n
-     d=n*2-1
-     while n > 0:
-      print(" "*s,"."*d)
-      n=n-1
+      L=n
+      L = int(L)
       s=L-n
       d=n*2-1
+      while n > 0:
+       print(" "*s,"."*d)       
+       n=n-1
+       s=L-n
+       d=n*2-1
     elif (n<=0):
-     print("please input a positive number")
+      print("please input a positive number")
+     
 except ValueError:
-    print("That's not an int!")
+    print("That's not an int!, pleas try again")
+   
 #n=int(n)
 '''if (n>0): 
     #print("Your input is number")

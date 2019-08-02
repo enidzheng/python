8fe109599a9db5bd93b719462a4a22b4b388ff01
Enid <52223959+enidzheng@users.noreply.github.com>
2019-08-02 10:15:43 +1000

    Add files via upload

--- secondhw.py
+++ secondhw.py
@@ -1,26 +1,32 @@
 import platform
 import random
+import traceback
 print("please input a number")
 R=input()
 i=0
-if R.isdigit():
-    while i< int(R):
-      for x in range(int(R)):
+try:
+    while R.isdigit() == True:
+      while i< int(R):
+        for x in range(int(R)):
           print(random.randint(1,10), end=' ') 
-      print("\n")
-      i+=1
-else:
-    while R!=int():
-        print("please try again, input a number")
-        R=int(input())
-        if R > 0:
-           i=0
-           while i< int(R):
-               for x in range(int(R)):
-                  print(random.randint(1,10), end=' ') 
-               print("\n")
-               i+=1
-        else:
-            if R<=0:
-                break
- 
+        print("\n")
+        i+=1
+      print ("please input a number big than 0")
+      R = input ()
+except Exception as e:
+     print("type error: " + str(e))
+     print(traceback.format_exc())
+      # else:
+      #   while R!=int():
+      #      print("please try again, input a number")
+      #      R=int(input())
+      #      if R > 0:
+      #         i=0
+      #         while i< int(R):
+      #            for x in range(int(R)):
+      #              print(random.randint(1,10), end=' ') 
+      #            print("\n")
+      #            i+=1
+      #      else:
+      #        if R<=0:
+      #           break
